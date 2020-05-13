6d9ae98b44be969fdd3467d22af12b6cd728700b
mxjensen55 <59321567+mxjensen55@users.noreply.github.com>
2020-05-12 22:23:45 -0500

    updating code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,8 @@
-// from data.js
+//create variable for data
+
 var tableData = data;
+
+//create function that already has table displayed
 
 function createTable(data){
     var tbody = d3.select("tbody");
@@ -7,8 +10,6 @@ function createTable(data){
     data.forEach(function(dateFilter){
         // console.log(dateFilter)
         var row = tbody.append("tr");
-
-    
 
     Object.values(dateFilter).forEach(function(value){
         // console.log(value);
@@ -22,65 +23,34 @@ function createTable(data){
 
 createTable(tableData);
 
-// select the button
+//select the button
 
-// var button = d3.select("#filter-btn");
+var inputButton = d3.select("#filter-btn");
 
-// //select the form
+inputButton.on("click", function(){
 
-// var form = d3.select(".form-group");
+    //prevent page from re-loading
 
-// //create variable for tbody
-
-// var tbody = d3.select("tbody");
-
-//Create event handlers
-
-// button.on("click", runEnter);
-// form.on("submit", runEnter);
-
-// Complete the event handler function for the form
-function runEnter() {
-
-    // Prevent the page from refreshing
     d3.event.preventDefault();
-    
-    // Select the input element and get the raw HTML node
+
+    //select input element and get raw html note
+
     var inputElement = d3.select("#datetime");
-  
-    // Get the value property of the input element
+
+    //get value property of input element
+
     var inputValue = inputElement.property("value");
-  
-    // console.log(inputValue);
-    // console.log(tableData);
 
-    //Filter the data by date given
+    console.log(inputValue);
+    console.log(tableData);
 
-    var filteredData = tableData.filter(function(t){
-        return t.datetime === inputValue
-      });
+    var filteredData= tableData.filter(d => d.datetime === inputValue)
+
+    console.log(filteredData)
+
+    createTable(filteredData)
     
-    //print the filtered data to the console
 
-    console.log(filteredData);
 
-    //loop through the filtered data and console.log each object of filtered data
+})
 
-    // filteredData.forEach(function(dateFilter){
-    //     console.log(dateFilter)
-    //     var row = tbody.append("tr");
-
-    createTable(filteredData);
-
-    // Object.entries(dateFilter).forEach(function([key, value]){
-    //     console.log(key, value);
-    //     var cell = tbody.append("td");
-    //     cell.text(value);
-    //     });
-        
-    // });
-    //Where does this go?? 
-    // list.html("");
-};
-
-d3.selectAll("#filter-btn").on('click', runEnter);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,60 +1,66 @@
 // from data.js
 var tableData = data;
 
-function createTable(data){
+function createTable(data) {
     var tbody = d3.select("tbody");
     tbody.html("");
-    data.forEach(function(dateFilter){
+    data.forEach(function(dateFilter) {
         // console.log(dateFilter)
         var row = tbody.append("tr");
-
-    
-
-    Object.values(dateFilter).forEach(function(value){
-        // console.log(value);
-        var cell = row.append("td");
-        cell.text(value);
+        Object.values(dateFilter).forEach(function(value) {
+            // console.log(value);
+            var cell = row.append("td");
+            cell.text(value);
         });
 
-});
+    });
 
 }
 
 createTable(tableData);
 
+
+
 var filters = {};
 
-function updateFilters(){
-    var changeElement=d3.select(this).select("input");
-    var elementValue=changeElement.property("value");
-    var filterId=changeElement.attr("id");
-    if (elementValue){
-        filters[filterId]=elementValue;
-    }
+function updateFilters() {
+    // var changeElement = d3.selectAll(this).select("input");
+    var changeElement = d3.selectAll("input").nodes()
+    console.log(changeElement)
+    changeElement.forEach(item => {
+        console.log(item.value)
+        var elementValue = item.value;
+        var searchField = item.id;
+        if (elementValue) {
+            filters[searchField] = elementValue;
+        } else {
+            delete searchField;
+        }
+    })
+    console.log(filters)
 
-    else {
-        delete filters[filterId];
-    }
     runEnter();
 }
+
 
 // Complete the event handler function for the form
 function runEnter() {
 
     d3.event.preventDefault();
-    
-    var filteredData=tableData;
-    Object.entries(filters).forEach(([key, value])=>{
-        filteredData= filteredData.filter(row => row[key]===value);
+    var filteredData = tableData;
+    //object.entries displays a key, value pair of the filtered data
+    Object.entries(filters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] === value);
     });
 
-  createTable(filteredData);
- 
-
-  
+    createTable(filteredData);
 
     console.log(filteredData);
 
 };
 
-d3.selectAll("#filter-btn").on('click', updateFilters);
+var inputButton = d3.select("#filter-btn")
+var form = d3.select("#form")
+
+inputButton.on("click", updateFilters)
+form.on("submit", updateFilters)
